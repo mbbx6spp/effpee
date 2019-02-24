38dd7804b6741ac1543ac6874469228ccaec6a9d
Susan Potter <me@susanpotter.net>
2019-02-24 16:06:36 -0600

    Export more useful symbols from Effpee module to reduce module imports across exercise sources

--- src/Effpee.hs
+++ src/Effpee.hs
@@ -1,12 +1,21 @@
 module Effpee
   ( todo
   , fromString
+  , Bool (..)
+  , Generic (..)
+  , Num (..)
+  , Show (..)
+  , Integer
   ) where
 
-import Data.Monoid (mconcat)
-import Data.String (String, fromString)
-import Data.Text   (Text, unpack)
-import GHC.Err     (error)
+import Data.Bool    (Bool (..))
+import Data.Monoid  (mconcat)
+import Data.String  (String, fromString)
+import Data.Text    (Text, unpack)
+import GHC.Err      (error)
+import GHC.Generics (Generic)
+import GHC.Num      (Integer, Num (..))
+import GHC.Show     (Show (..))
 
 todo :: String -> a
 todo s = error (mconcat ["TODO ", s])
