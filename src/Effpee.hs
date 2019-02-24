b84f8e34ad4d2e9b3d2a5774a8b4f37fcd0d33b5
Susan Potter <me@susanpotter.net>
2019-02-24 17:25:53 -0600

    Update Effpee module re-exported symbols

--- src/Effpee.hs
+++ src/Effpee.hs
@@ -6,16 +6,19 @@ module Effpee
   , Num (..)
   , Show (..)
   , Integer
+  , (<$>)
+  , (<*>)
   ) where
 
-import Data.Bool    (Bool (..))
-import Data.Monoid  (mconcat)
-import Data.String  (String, fromString)
-import Data.Text    (Text, unpack)
-import GHC.Err      (error)
-import GHC.Generics (Generic)
-import GHC.Num      (Integer, Num (..))
-import GHC.Show     (Show (..))
+import Control.Applicative ((<$>), (<*>))
+import Data.Bool           (Bool (..))
+import Data.Monoid         (mconcat)
+import Data.String         (String, fromString)
+import Data.Text           (Text, unpack)
+import GHC.Err             (error)
+import GHC.Generics        (Generic)
+import GHC.Num             (Integer, Num (..))
+import GHC.Show            (Show (..))
 
 todo :: String -> a
 todo s = error (mconcat ["TODO ", s])
