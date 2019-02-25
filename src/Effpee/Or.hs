7c5f6beb939559b15eae16a4ca910848b16a0b15
Susan Potter <me@susanpotter.net>
2019-02-24 20:45:43 -0600

    Update Effpee.Or module

--- src/Effpee/Or.hs
+++ src/Effpee/Or.hs
@@ -1,4 +1,10 @@
-module Effpee.Or
-  (
-  ) where
+{-# LANGUAGE TypeOperators #-}
 
+module Effpee.Or where
+
+import Effpee
+import Effpee.ADT
+
+type err \/ succ = Or err succ
+
+type Defaultable a = a \/ a
