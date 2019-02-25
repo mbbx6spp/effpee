e2e669264518ab65cd643c39f1215ac90906bbd4
Susan Potter <me@susanpotter.net>
2019-02-25 07:50:36 -0600

    Add type holes for missing test groups in Effpee.ManyTest module

--- src/Effpee/Many.hs
+++ src/Effpee/Many.hs
@@ -2,16 +2,39 @@
 
 module Effpee.Many
   ( Many (..)
-  , reverse
-  , length
+  , headOrDefault
+  , append
   , toList
   , fromList
+  , drop
+  , dropWhile
+  , take
+  , takeWhile
+  , partition
+  , zipWith
+  , foldR
+  , foldL
+  , product
+  , product'
+  , sum
+  , sum'
+  , length
+  , or
+  , or'
+  , and
+  , and'
+  , transform
+  , filter
+  , singleton
+  , flatMap
+  , flatten
+  , reverse
   ) where
 
 import Data.Int
 import Data.Function ((.))
 import Effpee
-import Effpee.ADT (Many (..))
+import Effpee.ADT (Many (..), Boolean)
 import GHC.Show
 
 -- only needed for REPL interactions
@@ -41,6 +64,90 @@ append
   -> Many a
   -> Many a
 append = todo "Effpee.Many.append"
+
+-- | Converts a @Many a@ to a @[a]@.
+--   Should retain order of elements and length property.
+-- >>> toList (1 :. (2 :. (3 :. Empty)))
+-- [1, 2, 3]
+-- >>> length (toList (1 :. Empty)) == length [1]
+-- True
+-- >>> fromList (toList (1 :. (2 :. (3 :. Empty)))) == (1 :. (2 :. (3 :. Empty)))
+-- True
+toList
+  :: Many a
+  -> [a]
+toList Empty = []
+toList (a :. as) = a : toList as
+
+-- | Converts a @[a]@ to a @Many a@.
+--   Should retain order of elements and length property.
+fromList
+  :: [a]
+  -> Many a
+fromList = todo "Effpee.Many.fromList"
+
+-- | Drop the first @Integer@ elements in the given @Many a@.
+-- >>> drop 5 Empty
+-- Empty
+-- >>> drop 3 (1 :. 2 :. Empty)
+-- Empty
+-- >>> drop 1 (1 :. 2 :. 3 :. 4 :. Empty)
+-- (2 :. 3 :. 4 :. Empty)
+-- Would this work with /infinite/ lists?
+drop :: Integer -> Many a -> Many a
+drop = todo "Effpee.Many.drop"
+
+-- | Drop the first elements in the given @Many a@ that do not satisfy the
+-- given predicate function @(a -> Bool)@.
+-- >>> let isOdd = \x -> x `rem` 2 == 1
+-- >>> dropWhile isOdd (13 :. 21 :. 32 :. 41 :. Empty)
+-- (32 :. 41 :. Empty)
+dropWhile :: (a -> Bool) -> Many a -> Many a
+dropWhile = todo "Effpee.Many.dropWhile"
+
+-- | Take the first @Integer@ elements from the given @Many a@.
+-- >>> take 5 (1 :. 2 :. Empty)
+-- Empty
+-- >>> take 3 $ fromList [1 .. 10]
+-- (1 :. 2 :. 3 :. Empty)
+take
+  :: Integer
+  -> Many a
+  -> Many a
+take = todo "Effpee.Many.take"
+
+-- | Take the first elements satisfying the given predicate function @(a -> Bool)@.
+-- >>> takeWhile isOdd (51 :. 53 :. 6 :. 55 :. Empty)
+-- (51 :. 53 :. Empty)
+takeWhile
+  :: (a -> Bool)
+  -> Many a
+  -> Many a
+takeWhile = todo "Effpee.Many.takeWhile"
+
+-- | Partition the first elements that satisfy the given predicate function
+-- @(a -> Bool)@ from the rest of the given @Many a@, returning a 2-tuple
+-- with the first series of matches in the first field of the 2-tuple and
+-- the remainder in the second field of the 2-tuple produced.
+-- >>> partition isOdd (1 :. 2 :. 3 :. 4 :. Empty)
+-- ((1 :. Empty), (2 :. 3 :. 4 :. Empty))
+partition
+  :: (a -> Bool)
+  -> Many a
+  -> (Many a, Many a)
+partition = todo "Effpee.Many.partition -- define in terms of take* and/or drop*"
+
+-- | Given a function @(a -> b -> c)@, a @Many a@, and a @Many b@ produce a @Many c@.
+-- >>> zipWith (+) (fromList [1 .. 5]) (fromList [1 .. 3])
+-- (1 :. 4 :. 6 :. Empty)
+-- >>> zipWith (*) Empty (fromList [1 .. 10])
+-- Empty
+zipWith
+  :: (a -> b -> c)
+  -> Many a
+  -> Many b
+  -> Many c
+zipWith = todo "Effpee.Many.zipWith -- define using explicit recursion"
 
 -- | fold from a seed value starting from the right most element in a @Many a@.
 -- >>> foldR (:.) Empty (1 :. 2 :. 3 :. Empty)
@@ -100,6 +207,30 @@ length
   -> Int
 length = todo "Effpee.Many.length -- define only in terms of foldR."
 
+-- | OR-ing a @Many Boolean@ together.
+-- >>> or (Nah :. Yeah :. Empty)
+-- Yeah
+-- >>> or Empty
+-- Nah
+or :: Many Boolean -> Boolean
+or = todo "Effpee.Many.or -- can you use foldR?"
+
+-- | Generalize @or@.
+or' :: (a -> Bool) -> Many a -> Bool
+or' = todo "Effpee.Many.or'"
+
+-- | AND-ing a @Many Boolean@ together.
+-- >>> and (Yeah :. Nah :. Yeah :. Empty)
+-- Nah
+-- >>> and Empty
+-- Nah
+and :: Many Boolean -> Boolean
+and = todo "Effpee.Many.and -- same question as above. Is there a pattern that you can extract from this?"
+
+-- | Generalize @and@.
+and' :: (a -> Bool) -> Many a -> Bool
+and' = todo "Effpee.Many.and'"
+
 -- | Transform each element based on the given function @(a -> b)@.
 -- >>> transform (+1) (1 :. 2 :. 3 :. Empty)
 -- (2 :. 3 :. 4 :. Empty)
@@ -158,30 +289,3 @@ reverse
   :: Many a
   -> Many a
 reverse = todo "Effpee.Many.reverse -- define in terms of explicit recursion AND foldR"
-
--- | Converts a @Many a@ to a @[a]@.
---   Should retain order of elements and length property.
--- >>> toList (1 :. (2 :. (3 :. Empty)))
--- [1, 2, 3]
--- >>> length (toList (1 :. Empty)) == length [1]
--- True
--- >>> fromList (toList (1 :. (2 :. (3 :. Empty)))) == (1 :. (2 :. (3 :. Empty)))
--- True
-toList
-  :: Many a
-  -> [a]
-toList Empty = []
-toList (a :. as) = a : toList as
-
--- | Converts a @[a]@ to a @Many a@.
---   Should retain order of elements and length property.
-fromList
-  :: [a]
-  -> Many a
-fromList = todo "Effpee.Many.fromList"
-
-drop :: Integer -> Many a -> Many a
-drop = todo "Effpee.Many.drop"
-
-dropWhile :: (a -> Bool) -> Many a -> Many a
-dropWhile = todo "Effpee.Many.dropWhile"
--- test/Effpee/ManyTest.hs
+++ test/Effpee/ManyTest.hs
@@ -1,17 +1,42 @@
 module Effpee.ManyTest (suite) where
 
 import Effpee.Test
+import Effpee
 
 -- module under test
 import Effpee.Many
 
 suite
   = testGroup "Many"
-    [ testProperty "reverse . reverse == identity" propReverseReverseIsId
-    , testProperty "reverse == identity when list empty" propReverseIsIdEmpty
-    , testProperty "reverse == identity when list singleton" propReverseIsIdSingleton
-    , testProperty "length . reverse == length . identity" propReverseLengthIsIdLength
-    ]
+  [ reverseTests
+  , headOrDefaultTests
+  , appendTests
+  , toListTests
+  , fromListTests
+  , dropTests
+  , dropWhileTests
+  , takeTests
+  , takeWhileTests
+  , partitionTests
+  , zipWithTests
+  , foldRTests
+  , foldLTests
+  , productTests
+  , product'Tests
+  , sumTests
+  , sum'Tests
+  , lengthTests
+  , orTests
+  , or'Tests
+  , andTests
+  , and'Tests
+  , transformTests
+  , filterTests
+  , singletonTests
+  , flatMapTests
+  , flattenTests
+  , reverseTests
+  ]
 
 genMany :: GenT Identity (Many Char)
 genMany = fromList <$> list (linear 0 100) alpha
@@ -24,6 +49,83 @@ genManyMany = fromList <$> list (linear 2 100) alpha
 
 genManySingleton :: GenT Identity (Many Char)
 genManySingleton = fromList <$> list (linear 1 1) alpha
+
+{-
+ _______________
+< headOrDefault >
+ ---------------
+        \   ^__^
+         \  (--)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+-}
+
+headOrDefaultTests = _
+
+appendTests = _
+
+toListTests = _
+
+fromListTests = _
+
+dropTests = _
+
+dropWhileTests = _
+
+takeTests = _
+
+takeWhileTests = _
+
+partitionTests = _
+
+zipWithTests = _
+
+foldRTests = _
+
+foldLTests = _
+
+productTests = _
+
+product'Tests = _
+
+sumTests = _
+
+sum'Tests = _
+
+lengthTests = _
+
+orTests = _
+
+or'Tests = _
+
+andTests = _
+
+and'Tests = _
+
+transformTests = _
+
+filterTests = _
+
+singletonTests = _
+
+{-
+ _________
+< reverse >
+ ---------
+        \   ^__^
+         \  (**)\_______
+            (__)\       )\/\
+             U  ||----w |
+                ||     ||
+-}
+
+reverseTests = testGroup "reverse"
+    [ testProperty "reverse . reverse == identity" propReverseReverseIsId
+    , testProperty "reverse == identity when list empty" propReverseIsIdEmpty
+    , testProperty "reverse == identity when list singleton" propReverseIsIdSingleton
+    , testProperty "length . reverse == length . identity" propReverseLengthIsIdLength
+    ]
 
 propReverseReverseIsId = property $ do
   xs <- forAll genMany
@@ -40,3 +142,9 @@ propReverseIsIdSingleton = property $ do
 propReverseLengthIsIdLength = property $ do
   xs <- forAll genMany
   length (reverse xs) === length xs
+
+
+
+flatMapTests = _
+
+flattenTests = _
